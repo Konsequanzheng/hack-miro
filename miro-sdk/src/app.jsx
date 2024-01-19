7404f3b61b33d636ff9046eec917851213417032
Thomas Walter <t.walter@tjip.com>
2024-01-19 22:13:00 +0100

    Add folder for 3D view and basics from the image gen app

--- miro-sdk/src/app.jsx
+++ miro-sdk/src/app.jsx
@@ -42,6 +42,6 @@ const App = () => {
   );
 };
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+//const container = document.getElementById('root');
+//const root = createRoot(container);
+//root.render(<App />);
