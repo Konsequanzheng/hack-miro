f29e1a42f2c3a011312fc83dc1a1c2785dddc3a4
Quan Zheng <q.zheng-7@student.tudelft.nl>
2024-01-20 01:16:07 +0100

    Implement Shap-e

--- ai-image-generator/pages/index.jsx
+++ ai-image-generator/pages/index.jsx
@@ -48,7 +48,7 @@ export default function Main() {
 
     // post our prompt to our backend
     try {
-      const response = await fetch("/api/openai", {
+      const response = await fetch("/api/replicate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
