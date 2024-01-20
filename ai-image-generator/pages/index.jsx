0c57b0ba5ede406ad6dc41eeea2cd6602870e07f
Quan Zheng <q.zheng-7@student.tudelft.nl>
2024-01-20 21:46:31 +0100

    Make the magic happen 🧙‍♂️

--- ai-image-generator/pages/index.jsx
+++ ai-image-generator/pages/index.jsx
@@ -3,42 +3,18 @@ import PromptInput from "../components/PromptInput";
 import Button from "../components/Button";
 import Renderer from "../components/Renderer";
 
- 
-
-async function checkIfAssetIsOverBox(magicboxOld) {
-  const items = await miro.board.get({
-    type: ['image', 'sticky_note'],
-  });
-  const magicbox = await miro.board.getById(magicboxOld.id);
-  if(magicbox === null) {
-    return;
-  }
-  console.log(items);
-
-  for(var index in items) {
-    const item = items[index];
-    if(Math.abs(item.x - magicbox.x) < item.width/2 + magicbox.width/2 
-    && Math.abs(item.y - magicbox.y) < item.height/2 + magicbox.height/2) {
-      if(item.type === "sticky_note") {
-        console.log(item.content);
-      }
-    }
-  }
-  
-}
-
 export default function Main() {
   const [inputValue, setInputValue] = useState("");
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
   const [asset, setAsset] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
-    
     window.miro.board.ui.on("icon:click", async () => {
       const magicBox = await miro.board.createImage({
-        title: 'Magic Box',
-        url: 'https://as1.ftcdn.net/v2/jpg/05/72/14/12/1000_F_572141234_oRsM7v29Ed0j1rYDcAhZwaO1VtBOSZaw.jpg',
+        title: "Magic Box",
+        url: "https://as1.ftcdn.net/v2/jpg/05/72/14/12/1000_F_572141234_oRsM7v29Ed0j1rYDcAhZwaO1VtBOSZaw.jpg",
         x: 0, // Default value: horizontal center of the board
         y: 0, // Default value: vertical center of the board
         width: 100, // Set either 'width', or 'height'
@@ -107,6 +83,42 @@ export default function Main() {
     setLoading(false);
   };
 
+  async function checkIfAssetIsOverBox(magicboxOld) {
+    const items = await miro.board.get({
+      type: ["image", "sticky_note"],
+    });
+    const magicbox = await miro.board.getById(magicboxOld.id);
+    if (magicbox === null) {
+      return;
+    }
+
+    for (var index in items) {
+      const item = items[index];
+      if (
+        Math.abs(item.x - magicbox.x) < item.width / 2 + magicbox.width / 2 &&
+        Math.abs(item.y - magicbox.y) < item.height / 2 + magicbox.height / 2
+      ) {
+        if (item.type === "sticky_note") {
+          const prompt = item.content.replace(/(<([^>]+)>)/gi, "");
+          console.log(prompt);
+          await miro.board.remove(item);
+          const response = await fetch("/api/openai", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: prompt }),
+          });
+          //get the response back from backend, which has the URL which we are looking for
+          const { data: imageUrl } = await response.json();
+
+          setAsset(imageUrl);
+          setTitle(prompt);
+        }
+      }
+    }
+  }
+
   const placeStickyNote = async () => {
     const position = await miro.board.experimental.findEmptySpace({
       x: 0,
@@ -124,6 +136,7 @@ export default function Main() {
     // }
     console.log(position);
     const image = await window.miro.board.createImage({
+      title: title,
       x: position.x,
       y: position.y,
       url: asset,
